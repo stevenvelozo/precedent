18e702c33bb91fe39c71d799da530a2af507c994
Steven Velozo <steven@velozo.com>
2023-02-20 16:49:21 -0800

    New web build, version bump

--- dist/precedent.js
+++ dist/precedent.js
@@ -278,25 +278,25 @@
          * @method addChild
          * @param {Object} pTree - A parse tree to push the characters into
          * @param {string} pPattern - The string to add to the tree
-         * @param {number} pIndex - callback function
+         * @param {number} pIndex - The index of the character in the pattern
          * @returns {Object} The resulting leaf node that was added (or found)
          * @private
          */
         addChild(pTree, pPattern, pIndex) {
-          if (pIndex > pPattern.length) return pTree;
           if (!pTree.hasOwnProperty(pPattern[pIndex])) pTree[pPattern[pIndex]] = {};
           return pTree[pPattern[pIndex]];
         }
 
         /** Add a Pattern to the Parse Tree
          * @method addPattern
-         * @param {Object} pTree - A node on the parse tree to push the characters into
-         * @param {string} pPattern - The string to add to the tree
-         * @param {number} pIndex - callback function
+         * @param {Object} pPatternStart - The starting string for the pattern (e.g. "${")
+         * @param {string} pPatternEnd - The ending string for the pattern (e.g. "}")
+         * @param {number} pParser - The function to parse if this is the matched pattern, once the Pattern End is met.  If this is a string, a simple replacement occurs.
          * @return {bool} True if adding the pattern was successful
          */
         addPattern(pPatternStart, pPatternEnd, pParser) {
           if (pPatternStart.length < 1) return false;
+          if (typeof pPatternEnd === 'string' && pPatternEnd.length < 1) return false;
           let tmpLeaf = this.ParseTree;
 
           // Add the tree of leaves iteratively
